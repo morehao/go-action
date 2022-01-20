02d6b7767de1ce80c686386fe38e64c1009a774b
morehao <morehao@qq.com>
2022-01-20 13:49:47 +0800

    feat(interview): 判断两个给定的字符串排序后是否一致

---
+++ interview/q4/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(isRegroup("abc", "ba"))
+}
+
+func isRegroup(s1, s2 string) bool {
+	sl1, sl2 := len([]rune(s1)), len([]rune(s2))
+	if sl1 > 5000 || sl2 > 5000 || sl1 != sl2 {
+		return false
+	}
+	for _, v := range s1 {
+		if strings.Count(s1, string(v)) != strings.Count(s2, string(v)) {
+			return false
+		}
+	}
+	return true
+}
