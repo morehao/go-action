0672382e5496404ffd814df9392ca3623ee45bfa
morehao <morehao@qq.com>
2022-02-17 23:22:20 +0800

    feat(time): ticker

--- time/ticker/main.go
+++ time/ticker/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	tickerDemo()
+	tickerLaunch()
 }
 
 func tickerDemo() {
@@ -16,4 +17,37 @@ func tickerDemo() {
 	for range ticker.C {
 		fmt.Println("tickerDemo")
 	}
+}
+
+func tickerLaunch() {
+	ticker := time.NewTicker(5 * time.Minute)
+	maxPassenger := 30 // 每车最大装载人数
+	passengers := make([]string, 0, maxPassenger)
+	for {
+		passenger := getNewPassenger() // 获取一个新乘客
+		if passenger != "" {
+			passengers = append(passengers, passenger)
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+		select {
+		case <-ticker.C:
+			launch(passengers)
+			passengers = []string{}
+		default:
+			if len(passengers) >= maxPassenger {
+				launch(passengers)
+				passengers = []string{}
+			}
+
+		}
+	}
+}
+
+func getNewPassenger() string {
+	return "passenger"
+}
+
+func launch(passengers []string) {
+	fmt.Println("The bus launch!")
 }
