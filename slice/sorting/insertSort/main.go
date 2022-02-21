8677518b406f342bf24ef39c7449cc9df3db5888
morehao <morehao@qq.com>
2022-02-21 13:25:05 +0800

    feat(sort): insert sort

---
+++ slice/sorting/insertSort/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr = []int{19, 8, 16, 15, 23, 34, 6, 3, 1, 0, 2, 9, 7}
+	insertSortAsc(arr)
+	fmt.Println(arr)
+	insertSortDesc(arr)
+	fmt.Println(arr)
+}
+
+// 降序插入排序
+func insertSortAsc(nums []int) {
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		currentItem := nums[i]
+		j := i
+		for j > 0 && nums[j-1] > currentItem {
+			nums[j] = nums[j-1]
+			j--
+		}
+		nums[j] = currentItem
+	}
+}
+
+// 升序插入排序
+func insertSortDesc(nums []int) {
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		currentItem := nums[i]
+		j := i
+		for j > 0 && nums[j-1] < currentItem {
+			nums[j] = nums[j-1]
+			j--
+		}
+		nums[j] = currentItem
+	}
+}
