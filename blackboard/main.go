7725ac87bb75664c5fbcc3af3c7e29d977d04b11
morehao <morehao@qq.com>
2022-02-23 09:43:33 +0800

    feat(practice): sort practice

--- blackboard/main.go
+++ blackboard/main.go
@@ -1,29 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
-	print()
+	var arr = []int{19, 8, 16, 15, 23, 34, 6, 3, 1, 0, 2, 9, 7}
+	sortArr(arr, 0, len(arr)-1)
+	fmt.Println("sortArr:", arr)
 }
 
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age" form:"age"`
-}
-
-func print() {
-	user := User{
-		Name: "张三",
-		Age:  18,
+func sortArr(arr []int, start, end int) {
+	if start < end {
+		i, j := start, end
+		key := arr[(start+end)/2]
+		for i <= j {
+			for arr[i] < key {
+				i++
+			}
+			for arr[j] > key {
+				j--
+			}
+			if i <= j {
+				arr[i], arr[j] = arr[j], arr[i]
+				i++
+				j--
+			}
+		}
+		if start < j {
+			sortArr(arr, start, j)
+		}
+		if end > i {
+			sortArr(arr, i, end)
+		}
 	}
-	v := reflect.ValueOf(&user)
-	v = v.Elem()
-	f := v.FieldByName("Name")
-	if f.Kind() == reflect.String {
-		f.SetString("kuteng")
-	}
-	fmt.Println(user)
 }
