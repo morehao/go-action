148c1f4367effaadd16227e284649ed7d0df1bea
morehao <morehao@qq.com>
2022-02-27 00:47:58 +0800

    feat(interview): 交替合并字符串代码优化

--- interview/mergeSreingAlternately/main.go
+++ interview/mergeSreingAlternately/main.go
@@ -10,7 +10,7 @@ func main() {
 }
 
 func mergeAlternately(str1, str2 string) string {
-	res := make([]uint8, 0, len(str1)+len(str2))
+	res := make([]byte, 0, len(str1)+len(str2))
 	for i := 0; i < len(str1) || i < len(str2); i++ {
 		if i < len(str1) {
 			res = append(res, str1[i])
