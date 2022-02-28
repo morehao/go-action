b8650fc0b9e5a7737f507eaeede066705dc32d0f
morehao <morehao@qq.com>
2022-02-28 22:41:17 +0800

    feat(blackboard): 最大子串长度

--- blackboard/main.go
+++ blackboard/main.go
@@ -2,37 +2,33 @@ package main
 
 import (
 	"fmt"
-
-	"go-practict/interview/binaryTree"
 )
 
 func main() {
-	arr := []int{3, 9, 20, 0, 0, 15, 7}
-	root := binaryTree.CreateBinaryTree(0, arr)
-
-	fmt.Println(levelOrderPrint(root))
+	s := "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(s))
 }
 
-func levelOrderPrint(root *binaryTree.TreeNode) [][]int {
-	var res [][]int
-	if root == nil {
-		return res
-	}
-	nodeList := []*binaryTree.TreeNode{root}
-	for i := 0; len(nodeList) > 0; i++ {
-		res = append(res, []int{})
-		var currentLevelNodes []*binaryTree.TreeNode
-		for j := 0; j < len(nodeList); j++ {
-			node := nodeList[j]
-			res[i] = append(res[i], node.Val)
-			if node.Left != nil {
-				currentLevelNodes = append(currentLevelNodes, node.Left)
-			}
-			if node.Right != nil {
-				currentLevelNodes = append(currentLevelNodes, node.Right)
-			}
+func lengthOfLongestSubstring(s string) int {
+	m := make(map[byte]int)
+	n := len(s)
+	rk, res := -1, 0
+	for i := 0; i < n; i++ {
+		if i != 0 {
+			delete(m, s[i-1])
 		}
-		nodeList = currentLevelNodes
+		for rk+1 < n && m[s[rk+1]] == 0 {
+			m[s[rk+1]]++
+			rk++
+		}
+		res = max(res, rk-i+1)
 	}
 	return res
+}
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
 }
