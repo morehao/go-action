f29520f6e6c8cb5323eded3e40756d1d58804864
morehao <morehao@qq.com>
2022-03-05 18:36:28 +0800

    feat(blackboard): 加一

--- blackboard/main.go
+++ blackboard/main.go
@@ -1,50 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	nums := []int{1, 3, -1, -3, 5, 3, 6, 7}
-	fmt.Println(maxSlidingWindow(nums, 3))
+	nums := []int{1, 9, 9}
+	fmt.Println(plusOne(nums))
 }
 
-func maxSlidingWindow(nums []int, k int) []int {
-	slideWindows := &HighQueue{
-		List: []int{},
+func plusOne(nums []int) []int {
+	for i := len(nums) - 1; i >= 0; i-- {
+		nums[i] = (nums[i] + 1) % 10
+		if nums[i] != 0 {
+			return nums
+		}
 	}
-	result := make([]int, 0)
-	for i := 0; i < k-1; i++ {
-		slideWindows.push(nums[i])
-	}
-	for i := k - 1; i < len(nums); i++ {
-		slideWindows.push(nums[i])
-		result = append(result, slideWindows.max())
-		slideWindows.shift(nums[i-k+1])
-	}
-	return result
-}
-
-type HighQueue struct {
-	List []int
-}
-
-func (q *HighQueue) push(v int) {
-	for len(q.List) > 0 && q.List[len(q.List)-1] < v {
-		q.List = q.List[:len(q.List)-1]
-	}
-	q.List = append(q.List, v)
-}
-
-func (q *HighQueue) shift(v int) {
-	if v == q.List[0] {
-		q.List = q.List[1:]
-	}
-}
-
-func (q *HighQueue) max() int {
-	if len(q.List) == 0 {
-		return 0
-	}
-	return q.List[0]
+	newNums := make([]int, len(nums)+1)
+	newNums[0] = 1
+	return newNums
 }
--- interview/plusOne/main.go
+++ interview/plusOne/main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 2, 8}
+	nums := []int{1, 9, 9}
 	fmt.Println(plusOne(nums))
 }
 func plusOne(nums []int) []int {
-	for i := len(nums) - 1; i > 0; i-- {
+	for i := len(nums) - 1; i >= 0; i-- {
 		nums[i] = (nums[i] + 1) % 10
-		if nums[i]%10 > 0 {
+		if nums[i] != 0 {
 			return nums
 		}
 	}
 	newNums := make([]int, len(nums)+1)
-	newNums = append(newNums, nums...)
+	newNums[0] = 1
 	return newNums
 }
