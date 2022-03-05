1a741f33273e988f89230946051e8dedee44836e
morehao <morehao@qq.com>
2022-03-05 11:51:36 +0800

    feat(blackboard): 斐波那切数列

--- blackboard/main.go
+++ blackboard/main.go
@@ -1,94 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	arr := []int{5, 3, 6, 2, 4, 0, 7}
-	tree := createBinaryTree(0, arr)
-	deleteNode(tree, 3)
-	fmt.Println(tree.levelOrder())
-
+	fmt.Printf("%d\n", fib(5))
+	fmt.Println("-----------分割符-------")
+	fmt.Printf("%d\n", fib2(5))
 }
 
-func deleteNode(root *treeNode, val int) *treeNode {
-	if root == nil {
-		return nil
+func fib(n int) int {
+	if n < 2 {
+		return n
 	}
-	if root.val > val {
-		root.left = deleteNode(root.left, val)
-	}
-	if root.val < val {
-		root.right = deleteNode(root.right, val)
-	}
-	if root.val == val {
-		if root.left == nil && root.right == nil {
-			root = nil
-			return root
-		}
-		if root.left == nil && root.right != nil {
-			root = root.right
-			return root
-		}
-		if root.left != nil && root.right == nil {
-			root = root.left
-			return root
-		}
-		if root.left != nil && root.right != nil {
-			left, right := root.left, root.right
-			tmp := root.right
-			for tmp.left != nil {
-				tmp = tmp.left
-			}
-			tmp.left = left
-			root = right
-			return root
-		}
-	}
-	return root
+	return fib(n-1) + fib(n-2)
 }
 
-func createBinaryTree(i int, nums []int) *treeNode {
-	if nums[i] == 0 {
-		return nil
+func fib2(n int) int {
+	if n < 2 {
+		return n
 	}
-	tree := &treeNode{
-		val: nums[i],
+	pre, current := 0, 1
+	for i := 2; i <= n; i++ {
+		next := pre + current
+		pre = current
+		current = next
 	}
-	if i < len(nums) && 2*i+1 < len(nums) {
-		tree.left = createBinaryTree(2*i+1, nums)
-	}
-	if i < len(nums) && 2*i+2 < len(nums) {
-		tree.right = createBinaryTree(2*i+2, nums)
-	}
-	return tree
-}
-
-func (t *treeNode) levelOrder() [][]int {
-	nodeList := make([]*treeNode, 0)
-	nodeList = append(nodeList, t)
-	res := make([][]int, 0)
-	for i := 0; len(nodeList) > 0; i++ {
-		res = append(res, []int{})
-		var currentNodeList []*treeNode
-		for j := 0; j < len(nodeList); j++ {
-			node := nodeList[j]
-			res[i] = append(res[i], node.val)
-			if node.left != nil {
-				currentNodeList = append(currentNodeList, node.left)
-			}
-			if node.right != nil {
-				currentNodeList = append(currentNodeList, node.right)
-			}
-		}
-		nodeList = currentNodeList
-	}
-	return res
-}
-
-type treeNode struct {
-	val   int
-	left  *treeNode
-	right *treeNode
+	return current
 }
