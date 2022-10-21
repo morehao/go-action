9b6db6753b9a91bb59cb2f56bb3e37f7966a5557
morehao <morehao@qq.com>
2022-10-21 18:19:48 +0800

    feat(string): 根据汉字拼音排序

--- blackboard/main.go
+++ blackboard/main.go
@@ -5,20 +5,7 @@ import "fmt"
 func main() {
 
 	// Creating slices
-	slice1 := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	var slice2 []int
-	slice3 := make([]int, 5)
-
-	// Before copying
-	fmt.Println("------------before copy-------------")
-	fmt.Printf("len=%-4d cap=%-4d slice1=%v\n", len(slice1), cap(slice1), slice1)
-	fmt.Printf("len=%-4d cap=%-4d slice2=%v\n", len(slice2), cap(slice2), slice2)
-	fmt.Printf("len=%-4d cap=%-4d slice3=%v\n", len(slice3), cap(slice3), slice3)
-
-	// Copying the slices
-	copy_1 := copy(slice2, slice1)
-	fmt.Println()
-	fmt.Printf("len=%-4d cap=%-4d slice1=%v\n", len(slice1), cap(slice1), slice1)
-	fmt.Printf("len=%-4d cap=%-4d slice2=%v\n", len(slice2), cap(slice2), slice2)
-	fmt.Println("Total number of elements copied:", copy_1)
+	var schoolIdList []uint64
+	schoolIdList = append(schoolIdList, 1)
+	fmt.Println(schoolIdList)
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/mozillazg/go-pinyin v0.19.0 // indirect
 	github.com/pkg/sftp v1.13.4
 	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e
+	golang.org/x/text v0.3.6
 )
