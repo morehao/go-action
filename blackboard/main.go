036ab9ffb173d3ea36bf9b10d80ae3027c8275eb
morehao <morehao@qq.com>
2023-05-22 17:38:46 +0800

    feat(blackboard): 解决冲突

--- blackboard/main.go
+++ blackboard/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	SliceExtend()
+}
 
-	// Creating slices
-	var schoolIdList []uint64
-	schoolIdList = append(schoolIdList, 1)
-	fmt.Println(schoolIdList)
+func SliceExtend() {
+	var slice []int
+	s1 := append(slice, 1, 2, 3)
+	fmt.Println(cap(s1))
+	s2 := append(s1, 4)
+	fmt.Println(&s1[0] == &s2[0])
 }
