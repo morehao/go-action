463a10ed44cb2a3937fd471cf11d59478a3be2df
morehao <morehao@qq.com>
2023-05-25 17:19:11 +0800

    feat(leetcode): 剑指 Offer 20. 表示数值的字符串

--- blackboard/main.go
+++ blackboard/main.go
@@ -7,9 +7,7 @@ func main() {
 }
 
 func SliceExtend() {
-	var slice []int
-	s1 := append(slice, 1, 2, 3)
-	fmt.Println(cap(s1))
-	s2 := append(s1, 4)
-	fmt.Println(&s1[0] == &s2[0])
+	var s []int
+	s = append(s, 1)
+	fmt.Println(s)
 }
--- leetcode/replaceSpace/main.go
+++ leetcode/replaceSpace/main.go
@@ -10,11 +10,18 @@ func main() {
 func replaceSpace(s string) string {
 	str := []byte(s)
 	res := make([]byte, 0, len(str))
-	for i := 0; i < len(str); i++ {
-		if str[i] == ' ' {
+	// for i := 0; i < len(str); i++ {
+	// 	if str[i] == ' ' {
+	// 		res = append(res, []byte("%20")...)
+	// 	} else {
+	// 		res = append(res, str[i])
+	// 	}
+	// }
+	for _, v := range str {
+		if v == ' ' {
 			res = append(res, []byte("%20")...)
 		} else {
-			res = append(res, str[i])
+			res = append(res, v)
 		}
 	}
 	return string(res)
