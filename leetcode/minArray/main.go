e8cf9f25308ac1d2a8cf55d6d5b457822356a1e6
morehao <morehao@qq.com>
2023-06-09 22:10:07 +0800

    feat(leetcode): 剑指 Offer 11. 旋转数组的最小数字

--- leetcode/minArray/main.go
+++ leetcode/minArray/main.go
@@ -7,7 +7,26 @@ func main() {
 	fmt.Println(minArray(nums))
 }
 
+// 二分法
 func minArray(numbers []int) int {
+	left, right := 0, len(numbers)-1
+	for left < right {
+		// "/" 代表向下取整除法，因此恒有left ≤ m < right
+		mid := (left + right) / 2
+		if numbers[mid] < numbers[right] {
+			right = mid
+		} else if numbers[mid] > numbers[right] {
+			left = mid + 1
+		} else {
+			// right=right−1 只需证明每次执行此操作后，旋转点 x 仍在 [left, right]区间内即可
+			right--
+		}
+	}
+	return numbers[left]
+}
+
+// 简单粗暴
+func minArray2(numbers []int) int {
 	if len(numbers) == 1 {
 		return numbers[0]
 	}
@@ -18,20 +37,4 @@ func minArray(numbers []int) int {
 		}
 	}
 	return min
-}
-
-func minArray2(numbers []int) int {
-	low := 0
-	high := len(numbers) - 1
-	for low < high {
-		mid := low + (high-low)/2
-		if numbers[mid] < numbers[high] {
-			high = mid
-		} else if numbers[mid] > numbers[high] {
-			low = mid + 1
-		} else {
-			high--
-		}
-	}
-	return numbers[low]
 }
