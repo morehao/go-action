9c90c27a98ffbfcf456af0e35f94241187b86488
morehao <morehao@qq.com>
2023-09-15 11:32:47 +0800

    feat(util): 树形结构调整

--- utils/transfer/tree.go
+++ utils/transfer/tree.go
@@ -18,41 +18,29 @@ type NodeTree struct {
 
 // type NodeTreeMap map[uint64]*NodeTree
 
-func BuildTree() []*NodeTree {
-	var items = []NodeItem{
-		{Id: 1, Pid: 0, Label: "1"},
-		{Id: 2, Pid: 1, Label: "1-2"},
-		{Id: 3, Pid: 1, Label: "1-3"},
-		{Id: 4, Pid: 3, Label: "1-3-4"},
-		{Id: 5, Pid: 0, Label: "5"},
-		{Id: 6, Pid: 5, Label: "5-6"},
-		{Id: 7, Pid: 6, Label: "5-6-7"},
-		{Id: 8, Pid: 6, Label: "5-6-8"},
-	}
+func BuildTree(nodes []NodeItem) []*NodeTree {
 	treeList := make([]*NodeTree, 0)
 	treeMap := make(map[uint64]*NodeTree)
-	for _, item := range items {
+	for _, node := range nodes {
 		treeItem := NodeTree{
 			NodeItem: NodeItem{
-				Id:    item.Id,
-				Pid:   item.Pid,
-				Label: item.Label,
+				Id:    node.Id,
+				Pid:   node.Pid,
+				Label: node.Label,
 			},
 			Children: make([]*NodeTree, 0),
 		}
 		// 根节点收集
-		if item.Pid == 0 {
+		if node.Pid == 0 {
 			treeList = append(treeList, &treeItem)
 		} else {
-			if item.Pid == 2 {
-				fmt.Println(2)
-			}
 			// 子节点收集
-			treeMap[item.Pid].Children = append(treeMap[item.Pid].Children, &treeItem)
-
+			if _, ok := treeMap[node.Pid]; ok {
+				treeMap[node.Pid].Children = append(treeMap[node.Pid].Children, &treeItem)
+			}
 		}
 		// 把节点映射到map表
-		treeMap[item.Id] = &treeItem
+		treeMap[node.Id] = &treeItem
 	}
 	jsonRes, _ := json.Marshal(treeList)
 	fmt.Println(string(jsonRes))
--- utils/transfer/tree_test.go
+++ utils/transfer/tree_test.go
@@ -3,5 +3,15 @@ package transfer
 import "testing"
 
 func Test_BuildTree(t *testing.T) {
-	BuildTree()
+	var nodes = []NodeItem{
+		{Id: 1, Pid: 0, Label: "1"},
+		{Id: 2, Pid: 1, Label: "1-2"},
+		{Id: 3, Pid: 1, Label: "1-3"},
+		{Id: 4, Pid: 3, Label: "1-3-4"},
+		{Id: 5, Pid: 0, Label: "5"},
+		{Id: 6, Pid: 5, Label: "5-6"},
+		{Id: 7, Pid: 6, Label: "5-6-7"},
+		{Id: 8, Pid: 6, Label: "5-6-8"},
+	}
+	BuildTree(nodes)
 }
