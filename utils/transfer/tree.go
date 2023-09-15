e3e82114a9365939cd02581e38d21363d658b410
morehao <morehao@qq.com>
2023-09-15 14:20:07 +0800

    feat(util): 构建树形结构，只展示有效叶子节点及其父节点

--- utils/transfer/tree.go
+++ utils/transfer/tree.go
@@ -6,9 +6,20 @@ import (
 )
 
 type NodeItem struct {
-	Id    uint64
-	Label string
-	Pid   uint64
+	Id     uint64
+	Label  string
+	Pid    uint64
+	Status bool
+}
+
+type NodeList []NodeItem
+
+func (l NodeList) ToMap() map[uint64]NodeItem {
+	m := make(map[uint64]NodeItem)
+	for _, v := range l {
+		m[v.Id] = v
+	}
+	return m
 }
 
 type NodeTree struct {
@@ -30,19 +41,65 @@ func BuildTree(nodes []NodeItem) []*NodeTree {
 			},
 			Children: make([]*NodeTree, 0),
 		}
+		treeMap[node.Id] = &treeItem
 		// 根节点收集
 		if node.Pid == 0 {
 			treeList = append(treeList, &treeItem)
-		} else {
-			// 子节点收集
-			if _, ok := treeMap[node.Pid]; ok {
-				treeMap[node.Pid].Children = append(treeMap[node.Pid].Children, &treeItem)
+		}
+	}
+	for _, node := range treeMap {
+		if node.Pid != 0 {
+			if parent, ok := treeMap[node.Pid]; ok {
+				parent.Children = append(parent.Children, node)
 			}
 		}
-		// 把节点映射到map表
-		treeMap[node.Id] = &treeItem
 	}
 	jsonRes, _ := json.Marshal(treeList)
 	fmt.Println(string(jsonRes))
 	return treeList
+}
+
+func BuildValidTree(nodes []NodeItem) []*NodeTree {
+	nodeMap := make(map[uint64]*NodeTree)
+	var roots []*NodeTree
+
+	// 创建所有节点
+	for i := range nodes {
+		nodeMap[nodes[i].Id] = &NodeTree{
+			NodeItem: nodes[i],
+			Children: make([]*NodeTree, 0),
+		}
+	}
+
+	// 构建树
+	for i := range nodes {
+		node := nodeMap[nodes[i].Id]
+		if nodes[i].Pid == 0 {
+			roots = append(roots, node)
+		} else {
+			parentNode := nodeMap[nodes[i].Pid]
+			parentNode.Children = append(parentNode.Children, node)
+		}
+	}
+
+	// 删除状态为false的叶子节点
+	var prune func(node *NodeTree) bool
+	prune = func(node *NodeTree) bool {
+		for i := 0; i < len(node.Children); i++ {
+			if prune(node.Children[i]) {
+				node.Children = append(node.Children[:i], node.Children[i+1:]...)
+				i--
+			}
+		}
+		return len(node.Children) == 0 && !node.Status
+	}
+	for i := 0; i < len(roots); i++ {
+		if prune(roots[i]) {
+			roots = append(roots[:i], roots[i+1:]...)
+			i--
+		}
+	}
+	jsonRes, _ := json.Marshal(roots)
+	fmt.Println(string(jsonRes))
+	return roots
 }
--- utils/transfer/tree_test.go
+++ utils/transfer/tree_test.go
@@ -1,6 +1,9 @@
 package transfer
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_BuildTree(t *testing.T) {
 	var nodes = []NodeItem{
@@ -12,6 +15,22 @@ func Test_BuildTree(t *testing.T) {
 		{Id: 6, Pid: 5, Label: "5-6"},
 		{Id: 7, Pid: 6, Label: "5-6-7"},
 		{Id: 8, Pid: 6, Label: "5-6-8"},
+		{Id: 9, Pid: 8, Label: "5-6-8-9"},
+		{Id: 10, Pid: 9, Label: "5-6-8-9-10"},
 	}
 	BuildTree(nodes)
+}
+
+func Test_BuildValidTree(t *testing.T) {
+	var nodes = NodeList{
+		{Id: 1, Pid: 0, Label: "1"},
+		{Id: 2, Pid: 1, Label: "1-2", Status: true},
+		{Id: 3, Pid: 1, Label: "1-3", Status: true},
+		{Id: 4, Pid: 3, Label: "1-3-4"},
+		{Id: 5, Pid: 0, Label: "5"},
+		{Id: 6, Pid: 5, Label: "5-6"},
+		{Id: 7, Pid: 6, Label: "5-6-7"},
+		{Id: 8, Pid: 6, Label: "5-6-8", Status: true},
+	}
+	fmt.Println("tree:", BuildValidTree(nodes))
 }
