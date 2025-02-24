1277e8d42400ba69fd5240a66037533fc62efdee
morehao <morehao@qq.com>
2025-02-24 21:01:56 +0800

    feat(leetcode): 92. 反转链表 II

---
+++ leetcode/editor/cn/[92]反转链表 II.go
@@ -0,0 +1,68 @@
+/**
+给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节
+点，返回 反转后的链表 。
+
+
+
+ 示例 1：
+
+
+输入：head = [1,2,3,4,5], left = 2, right = 4
+输出：[1,4,3,2,5]
+
+
+ 示例 2：
+
+
+输入：head = [5], left = 1, right = 1
+输出：[5]
+
+
+
+
+ 提示：
+
+
+ 链表中节点数目为 n
+ 1 <= n <= 500
+ -500 <= Node.val <= 500
+ 1 <= left <= right <= n
+
+
+
+
+ 进阶： 你可以使用一趟扫描完成反转吗？
+
+ Related Topics 链表 👍 1923 👎 0
+
+*/
+
+package main
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+	tempNode := &ListNode{Val: -1}
+	tempNode.Next = head
+	pre := tempNode
+	// 先移动到待反转的第一个节点
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+	cur := pre.Next
+	for i := 0; i < right-left; i++ {
+		next := cur.Next
+		cur.Next = next.Next
+		next.Next = pre.Next
+		pre.Next = next
+	}
+	return tempNode.Next
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
