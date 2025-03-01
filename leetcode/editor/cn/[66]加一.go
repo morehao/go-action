4f85198a7153456d97a0171adcfa26299b5e742d
morehao <morehao@qq.com>
2025-03-01 17:03:28 +0800

    feat(leetcode): 66. 加一

---
+++ leetcode/editor/cn/[66]加一.go
@@ -0,0 +1,64 @@
+/**
+给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+
+ 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+ 你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+
+
+ 示例 1：
+
+
+输入：digits = [1,2,3]
+输出：[1,2,4]
+解释：输入数组表示数字 123。
+
+
+ 示例 2：
+
+
+输入：digits = [4,3,2,1]
+输出：[4,3,2,2]
+解释：输入数组表示数字 4321。
+
+
+ 示例 3：
+
+
+输入：digits = [9]
+输出：[1,0]
+解释：输入数组表示数字 9。
+加 1 得到了 9 + 1 = 10。
+因此，结果应该是 [1,0]。
+
+
+
+
+ 提示：
+
+
+ 1 <= digits.length <= 100
+ 0 <= digits[i] <= 9
+
+
+ Related Topics 数组 数学 👍 1470 👎 0
+
+*/
+
+package main
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i] = (digits[i] + 1) % 10
+		if digits[i] != 0 {
+			return digits
+		}
+	}
+	res := make([]int, len(digits)+1)
+	res[0] = 1
+	return res
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
