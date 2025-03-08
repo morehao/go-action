4a5ce29ca8f1a35b134183c300c84ee7e3825e46
morehao <morehao@qq.com>
2025-03-08 16:02:34 +0800

    feat(leetcode): LCR 079. 子集

---
+++ leetcode/editor/cn/[LCR 079]子集.go
@@ -0,0 +1,62 @@
+/**
+给定一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+
+ 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+
+
+
+ 示例 1：
+
+
+输入：nums = [1,2,3]
+输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+
+ 示例 2：
+
+
+输入：nums = [0]
+输出：[[],[0]]
+
+
+
+
+ 提示：
+
+
+ 1 <= nums.length <= 10
+ -10 <= nums[i] <= 10
+ nums 中的所有元素 互不相同
+
+
+
+
+
+ 注意：本题与主站 78 题相同： https://leetcode-cn.com/problems/subsets/
+
+ Related Topics 位运算 数组 回溯 👍 93 👎 0
+
+*/
+
+package main
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func subsets(nums []int) [][]int {
+	// 初始子集为空集
+	result := make([][]int, 1)
+
+	for _, num := range nums {
+		n := len(result)
+		for i := 0; i < n; i++ {
+			// 复制当前子集并加入新元素
+			newSubset := make([]int, len(result[i]))
+			copy(newSubset, result[i])
+			newSubset = append(newSubset, num)
+			result = append(result, newSubset)
+		}
+	}
+
+	return result
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
