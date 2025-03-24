7dcddbc571bc1052a28a7a676fecd24cd5f29bf4
morehao <morehao@qq.com>
2025-03-24 23:58:31 +0800

    feat(leetcode): 142. 环形链表 II

--- leetcode/editor/cn/[141]环形链表.go
+++ leetcode/editor/cn/[141]环形链表.go
@@ -74,16 +74,21 @@ package main
  * }
  */
 func hasCycle(head *ListNode) bool {
-	m := make(map[*ListNode]struct{})
-	for head != nil {
-		if _, ok := m[head]; ok {
-			return true
-		}
-		m[head] = struct{}{}
-		head = head.Next
-
+	if head == nil || head.Next == nil {
+		return false
 	}
-	return false
+	slow, fast := head, head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
 }
+
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
 
 // leetcode submit region end(Prohibit modification and deletion)
